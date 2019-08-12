cf8aa60c6cfd8daa831e27fc003d5812db8fca0b
Александр Гунгер <alexandergunger666@gmail.com>
2019-08-12 16:34:53 +0300

    Add BottomButton

---
+++ GuessMaster/widgets/BottomButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GuessMaster.widgets
+{
+    public class BottomButton : Button
+    {
+        public BottomButton()
+        {
+            Margin = new Thickness(10, 30, 10, 30);
+            FontSize = 18;
+        }
+    }
+}
