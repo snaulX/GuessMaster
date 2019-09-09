34dcfa63a15dcf0f5a5f1d5d5b3b36833e0e4cc3
Александр Гунгер <alexandergunger666@gmail.com>
2019-09-09 08:43:54 +0300

    Create Question class

---
+++ GuessMaster/main/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMaster.main
+{
+    public class Question
+    {
+        public Dictionary<string, bool> cases;
+        public string question;
+
+        public Question()
+        {
+            question = "";
+            cases = new Dictionary<string, bool>();
+        }
+
+        public string ToByteCode()
+        {
+            return "";
+        }
+    }
+}
