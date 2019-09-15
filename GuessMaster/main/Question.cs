634ced84dbfe4db0c53094c14c08a5dd203e5a62
Александр Гунгер <alexandergunger666@gmail.com>
2019-09-15 21:08:11 +0300

    Start filling method who get question from bytecode

--- GuessMaster/main/Question.cs
+++ GuessMaster/main/Question.cs
@@ -21,7 +21,7 @@ namespace GuessMaster.main
         {
             get
             {
-                string result = "<:::\00xQ::" + question + ':' + cases.Count + ";\0";
+                string result = "<:::\00xQ::" + question + "\0:" + cases.Count + ";\0";
                 foreach (KeyValuePair<string, bool> pair in cases)
                 {
                     result += "0xQQ:::" + pair.Key + ":::(a)\0-\0";
@@ -36,6 +36,23 @@ namespace GuessMaster.main
                     result += "\0%;";
                 }
                 return result + "kjlkm:::>";
+            }
+        }
+
+        public Question(string code)
+        {
+            if (code.StartsWith("<:::\00xQ::"))
+            {
+                code = code.Remove(0, 10); //remove '<:::\00xQ::' - start value
+                question = code.Substring(0, code.IndexOf('\0')); //get question
+                code = code.Remove(0, code.IndexOf('\0') + 1); //remove question and '\0'
+                if (code[0] != ':') throw new FormatException("Continue after question is not found");
+                code = code.Remove(0, 1); //remove ':'
+
+            }
+            else
+            {
+                throw new FormatException("Start of bytecode is not right");
             }
         }
     }
